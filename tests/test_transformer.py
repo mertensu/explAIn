d4b069f7e4ac56bd9c94ef57fb35ac820a6e76ef
mertensu <mertens.ulf@gmail.com>
2019-09-02 13:22:37 +0200

    Add docstring for test_transformer

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -4,6 +4,10 @@ from models import FeatureTransformer,RegressionLearner
 
 
 def test_transformer():
+    """
+    Test if class FeatureTransformer returns the correct shape.
+    :return: None
+    """
     inp = np.empty(shape=(64,1))
     transformer = FeatureTransformer()
     assert transformer(inp).numpy().shape == (64,1)
