d51d895188a349d419d69969719bca2cfb139f9e
mertensu <mertens.ulf@gmail.com>
2019-09-02 13:29:09 +0200

    Move RegressionLearner test to pytest

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ import pytest
 
 if __name__ == '__main__':
     tf.enable_eager_execution()
-
     pytest.main()
 
-    dataset = np.empty(shape=(64, 5))
-    reglearner = RegressionLearner(5)
 
-    predictions = reglearner(dataset)
-    print(predictions.shape)
+
 
 
