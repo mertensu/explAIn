8182bc7592309949a863bc3f1f019746e565cc9e
mertensu <mertens.ulf@gmail.com>
2019-09-03 11:17:44 +0200

    remove enable_eager due to TF version 2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
-import numpy as np
-from models import RegressionLearner
 import pytest
 
+
 if __name__ == '__main__':
-    tf.enable_eager_execution()
     pytest.main()
+
+
 
 
 
