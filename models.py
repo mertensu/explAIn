f00132dcd64633e721ec925f1f08a4930c4085dd
mertensu <mertens.ulf@gmail.com>
2019-09-06 10:07:22 +0200

    Change hidden activation to elu.

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class FeatureTransformer(tf.keras.Model):
         """
         super(FeatureTransformer, self).__init__()
         self.model = tf.keras.Sequential(
-            [tf.keras.layers.Dense(n_units, activation='sigmoid')
+            [tf.keras.layers.Dense(n_units, activation='elu')
              for i in range(n_layers)] +
             [tf.keras.layers.Dense(1,activation='sigmoid')]
         )
